3be589e5c989fff7b07c9a630db64a1f87b93b08
shaheenp <shaheenspage@gmail.com>
2017-05-07 21:50:33 -0600

    add i3 config

---
+++ home/.config/i3/start-gnome-keyring.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
+
---
+++ home/.config/i3/volume.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$1" == "decrease" ]
+then
+    amixer -q set Master 6.3%- unmute
+    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f6 | sed 's/\(\[\|\]\)//g')
+elif [ "$1" == "increase" ]
+then
+    amixer -q set Master 6.3%+ unmute
+    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f6 | sed 's/\(\[\|\]\)//g')
+elif [ "$1" == "toggle-mute" ]
+then
+    amixer -q set Master toggle
+    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f8 | sed 's/\(\[\|\]\)//g')
+else
+    echo "Usage volume [decrease | increase | toggle-mute]\n"
+fi
+
+ID=$(cat /home/shaheen/.config/i3/.dunst_volume)
+if [ $ID > 0 ]
+then
+    dunstify -t 1 -p -r $ID "Volume: $TEXT" > /home/shaheen/.config/i3/.dunst_volume
+else
+    dunstify -t 1 -p "Volume: $TEXT" > /home/shaheen/.config/i3/.dunst_volume
+fi
+
