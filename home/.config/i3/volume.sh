f5eb0c8c63f52ac1f41f23cc7c8b50e5ff76d39e
shaheenp <shaheenspage@gmail.com>
2017-11-30 20:08:31 -0800

    refactor volume.sh

--- home/.config/i3/volume.sh
+++ home/.config/i3/volume.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-if [ "$1" == "decrease" ]
-then
-    amixer -q set Master 6.25%- unmute
-    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f6 | sed 's/\(\[\|\]\)//g')
-elif [ "$1" == "increase" ]
-then
-    amixer -q set Master 6.25%+ unmute
-    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f6 | sed 's/\(\[\|\]\)//g')
-elif [ "$1" == "toggle-mute" ]
-then
-    amixer -q set Master toggle
-    TEXT=$(amixer sget Master | tail -1 | cut -d' ' -f8 | sed 's/\(\[\|\]\)//g')
-else
-    echo "Usage volume [decrease | increase | toggle-mute]\n"
-fi
+getText() {
+    local FIELD=$1
+    amixer sget Master | tail -1 | cut -d' ' -f${FIELD} | sed 's/\(\[\|\]\)//g'
+}
 
-ID=$(cat ~/.config/i3/.dunst_volume)
-if [ $ID > 0 ]
-then
-    dunstify -t 1 -p -r $ID "Volume: $TEXT" > ~/.config/i3/.dunst_volume
-else
-    dunstify -t 1 -p "Volume: $TEXT" > ~/.config/i3/.dunst_volume
-fi
+main() {
+
+    # range is 0-127; 127/16 ~= 8
+    local INT=8
+    local ACTION=$1
+    local DEC="decrease"
+    local INC="increase"
+    local MUTE="toggle-mute"
+
+    if [ $ACTION == $DEC ]
+    then
+        amixer -q set Master ${INT}- unmute
+        local TEXT=$(getText 6)
+    elif [ $ACTION == $INC ]
+    then
+        amixer -q set Master ${INT}+ unmute
+        local TEXT=$(getText 6)
+    elif [ $ACTION == $MUTE ]
+    then
+        amixer -q set Master toggle
+        if [ $(getText 8) == "on" ]
+        then
+            local TEXT=$(getText 6)
+        else
+            local TEXT="muted"
+        fi
+    else
+        echo "Usage volume [$DEC | $INC | $MUTE]\n"
+    fi
+
+    local ID=$(cat ~/.config/i3/.dunst_volume)
+    if [ $ID > 0 ]
+    then
+        dunstify -t 1 -p -r $ID "Volume: $TEXT" > ~/.config/i3/.dunst_volume
+    else
+        dunstify -t 1 -p "Volume: $TEXT" > ~/.config/i3/.dunst_volume
+    fi
+
+}
+
+main $@
 
